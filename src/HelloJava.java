d0453eca441b2c278629029b1d401862646ca68c
villiejam <villejam@yandex.ru>
2024-01-04 15:05:30 +0300

    added new class, with another way to create text in window

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -3,14 +3,12 @@ import javax.swing.*;
 public class HelloJava {
     public static void main( String[] args ) {
         JFrame frame = new JFrame( "Hello, Java!" );
-        JFrame frame1 = new JFrame( "VAZHNO" );
-        JLabel label = new JLabel( "Voobsche tut mojet bit' lyuboi yazik.", JLabel.CENTER );
-        JLabel label1 = new JLabel("No ya hotel skazat chto ya tebya lyublyu!", JLabel.CENTER );
-        frame.add( label );
-        frame1.add( label1 );
-        frame.setSize( 300, 300 );
-        frame1.setSize( 300, 300 );
+
+        JLabel label = new JLabel( "<html>Voobsche tut mojet bit' lyuboi yazik.<br/>No ya hotel skazat', chto ya lyublyu tebya!<html/>", JLabel.CENTER );
+        frame.add( new HelloComponent() );
+        frame.setSize( 500, 500 );
+
         frame.setVisible( true );
-        frame1.setVisible( true );
+
     }
 }
